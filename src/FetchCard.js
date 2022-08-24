94e35b13a94eee65a846e1f18fb82d91b3c7bd9b
fabnupi23 <31360808+fabnupi23@users.noreply.github.com>
2022-08-24 10:07:03 -0500

    Extraer datos de la API por medio de funciones

--- src/FetchCard.js
+++ src/FetchCard.js
@@ -1,13 +1,20 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { getUser } from './helpers/getUser';
+import { getPosts } from './helpers/getPosts';
 
-const initialUser = {
+/* const initialUser = {
     name: "Fabian",
     email: "fabnupi023@gmail.com"
 }
 
+const initialPosts = [
+    {id: 1, title: "Post 1"},
+    {id: 2, title: "Post 2"},
+] */
+
 const FetchCard = () => {
-    const [user, setUser] = useState(initialUser);
+    const [user, setUser] = useState({});
+    const [posts, setsPosts] = useState([]);
 
     const updateUser = () => {
         getUser()
@@ -16,18 +23,42 @@ const FetchCard = () => {
             })
     }
 
+    const updatePost = useCallback (() => {
+        getPosts(user.id)
+            .then((newPosts) => {
+                setsPosts(newPosts);
+            })
+    }, [user.id]);
+
+    
+
+
     useEffect(() => {
         updateUser();
     }, []);
+
+    useEffect(() => {
+        if(user?.id){
+            updatePost();
+        }        
+    }, [user, updatePost]);
 
   return (
     <div>
         <h1>{user.name}</h1>
         <h1>{user.email}</h1>
+
+        <h2>Posts - user: {user.id}</h2>
+        <ul>
+            {posts.map(post => (
+                <li key={post.id}>{post.title}</li>
+            ))}
+        </ul>
+        <br/> 
         <button onClick={updateUser}>
-            Cargar Usuario
-        </button>
-        
+            Cargar Usuario - POST
+        </button>         
+
     </div>
   )
 }
